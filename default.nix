978ef9bd7956846cf2a2bb8aeb72b80449889e83
Abraham Vallejos <abraham-rat@outlook.es>
2024-01-28 16:49:28 -0500

    [Abraham]:added file default.nix for deploy and run in railway.

--- default.nix
+++ default.nix
@@ -2,15 +2,12 @@
 
 pkgs.mkShell {
   buildInputs = with pkgs; [
-    php80
+    php
     composer
     git
   ];
 
   shellHook = ''
     export COMPOSER_HOME=$TMPDIR
-    composer create-project laravel/laravel:^4.5.1 my-laravel-app --prefer-dist --no-progress --no-interaction
-    cd my-laravel-app
-    php artisan key:generate
   '';
 }
